2af589212a44c6dfd9dab6db1b7f989717339d3e
Dev Jana <dev@primeacademy.io>
2018-09-07 14:53:24 -0500

    basic messaging working

--- server/modules/routes/messages.js
+++ server/modules/routes/messages.js
@@ -1,11 +1,18 @@
 // a router that uses express... otherwise known as an "Express Router"
 const express = require( 'express' );
 const router = express.Router();
-
+// globals
+let messages = [];
 router.get( '/', ( req, res )=>{
     console.log( '/messages GET router hit' );
-    res.send( 'whinney' );
+    res.send( messages );
 }) // end GET call
+
+router.post( '/', ( req, res )=>{
+    console.log( '/messages POST router hit:', req.body );
+    messages.push( req.body );
+    res.sendStatus( 200 );
+}) // end POST call
 
 // module exports
 module.exports = router;
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,53 @@
 $( document ).ready( readyNow );
 
+function login(){
+    console.log( 'in login' );
+} // end login
+
+function logout(){
+    console.log( 'in logout' );
+} // end logout
+
 function readyNow(){
-    console.log( 'JQ' );
+    $( '#loginButton' ).on( 'click', login );
+    $( '#logoutButton' ).on( 'click', logout );
+    $( '#submitButton' ).on( 'click', submit );
+    updateMessages();
 } // end readynow
+
+function submit(){
+    console.log( 'in submit' );
+    let objectToSend = {
+        from: $( '#usernameIn' ).val(),
+        text: $( '#messageIn' ).val()
+    } //end objectToSend
+    console.log( 'objectToSend:', objectToSend );
+    $.ajax({
+        method: 'POST',
+        url: '/messages',
+        data: objectToSend
+    }).then( function( response ){
+        console.log( 'back from POST with:', response );
+        updateMessages();
+    }).catch( function( error ){
+        alert( 'Error posting new message' );
+        console.log( 'error:', error );
+    }) //end ajax
+} // end submit
+
+function updateMessages(){
+    $.ajax({
+        method: 'GET',
+        url: '/messages'
+    }).then( function ( response ){
+        console.log( 'back from GET with:', response );
+        let el = $( '#messagesOut' );
+        el.empty();
+        for( message of response ){
+            el.append( `<li>${message.text}: <i>${message.from}</i></li>`)
+        } //end for
+    }).catch( function( error ){
+        alert( 'Error updating messages' );
+        console.log( 'error:', error );
+    }) //end ajax
+} // end updateMessages
